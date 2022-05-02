ddd19fbef4b2f60721d9bee6d9c26bd8ed4970fb
arie <arie478@gmail.com>
2022-05-02 17:48:54 +0300

    Added Guest class

---
+++ src/Domain/Guest.java
@@ -0,0 +1,20 @@
+package Domain;
+
+import Service.UserApplication;
+
+public class Guest
+{
+    /** -----Base attributes----- */
+
+    /** -----Derivatives attributes----- */
+    private UserApplication userApplication;
+
+    /** -----Functions----- */
+
+
+    public void register(String userName, String password, String email)
+    {
+        //TODO - Implement function + change return type
+    }
+
+}
