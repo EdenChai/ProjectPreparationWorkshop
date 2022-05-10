d03d2e8ad78ed8375dc8e021f18827208e56e50f
arie <arie478@gmail.com>
2022-05-10 15:33:12 +0300

    Added test example class with basic functions Added Frame for unit testing

---
+++ Tests/UnitTesting/AssociationMemberTest.java
@@ -0,0 +1,56 @@
+package UnitTesting;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssociationMemberTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void makeNewLeague()
+    {
+    }
+
+    @Test
+    void addSeasonToLeague()
+    {
+    }
+
+    @Test
+    void addReferee()
+    {
+    }
+
+    @Test
+    void assignReferee()
+    {
+    }
+
+    @Test
+    void addPolicy()
+    {
+    }
+
+    @Test
+    void editPolicy()
+    {
+    }
+
+    @Test
+    void activateGameAssignment()
+    {
+    }
+}
